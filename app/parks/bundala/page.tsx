c06c6c30e86907480d63cdfb4f954225830f898b
rumeshsmrr <siriwardhanarumesh@gmail.com>
2025-07-30 22:26:10 +0530

    Add metadata for Bundala National Park page to enhance SEO

--- app/parks/bundala/page.tsx
+++ app/parks/bundala/page.tsx
@@ -8,6 +8,26 @@ import Gallery from "@/app/Components/Gallery";
 import Features from "../Features";
 import Nav from "@/app/Components/Nav";
 import FreePick from "../freePick";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Bundala National Park - Nimal Safari",
+  description:
+    "Explore the serene beauty of Bundala National Park, a UNESCO biosphere reserve known for its rich biodiversity and stunning landscapes. Book your safari today!",
+  icons: {
+    icon: "/favicon.ico", // or "/favicon.png"
+  },
+  keywords: [
+    "Bundala National Park",
+    "Safari",
+    "Wildlife",
+    "Sri Lanka",
+    "Nimal Safari",
+    "birdwatching",
+    "nature tours",
+    "wildlife photography",
+  ],
+};
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
