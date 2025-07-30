f5971d30529ed1c89c99c6ef4b15a4fd4a602bc2
rumeshsmrr <siriwardhanarumesh@gmail.com>
2025-07-30 22:28:04 +0530

    Add metadata for Udawalawa National Park page to enhance SEO

--- app/parks/udawalawa/page.tsx
+++ app/parks/udawalawa/page.tsx
@@ -9,6 +9,32 @@ import Features from "../Features";
 import Nav from "../../Components/Nav";
 import FreePick from "../freePick";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Udawalawa National Park | Nimal Safari",
+  description:
+    "Explore Udawalawa National Park, a premier destination for wildlife enthusiasts. Experience the thrill of seeing elephants in their natural habitat and enjoy a unique safari adventure.",
+  icons: {
+    icon: "/favicon.ico", // or "/favicon.png"
+  },
+  keywords: [
+    "Udawalawa National Park",
+    "Safari",
+    "Wildlife",
+    "Sri Lanka",
+    "Nimal Safari",
+    "nature safari",
+    "Sri Lanka safari",
+    "Elephant Sanctuary",
+    "conservation",
+    "safari tours",
+    "birdwatching",
+    "nature tours",
+    "wildlife photography",
+  ],
+};
+
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
